7ee0eef68cc5efb15f3bc8eda037fbd643906ce6
yxy <yxy360@gmail.com>
2025-01-03 23:35:33 -0500

    restructure file

---
+++ PLAYGAME.py
@@ -0,0 +1,37 @@
+import os
+import pygame
+import game_func
+from settings import Settings
+from button import Button
+
+def run_game():
+    pygame.init()
+    pygame.mixer.init()
+
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pygame.mouse.set_visible(False)
+
+    settings = Settings()
+    screen = settings.screen
+    pygame.display.set_caption('Battle Chess')
+    
+    settings.play_button = Button(screen,settings,'Play')
+    chesses = []
+    dead_chesses = []
+    game_func.create_whole_chess(settings,chesses,screen)
+    settings.chesses = chesses[:]
+
+    while True:
+        for event in pygame.event.get():
+            if not settings.game_end and not settings.game_rewind:
+                game_func.check_event_ingame(chesses,settings,dead_chesses,screen,event)
+            if settings.game_rewind:
+                game_func.check_event_gamerewind(chesses,settings,dead_chesses,screen,event)
+            if settings.game_end:
+                game_func.check_event_gameend(chesses,settings,dead_chesses,screen,event)
+            game_func.check_event_display(chesses,settings,dead_chesses,screen,event)
+
+        game_func.update_screen(settings,screen,chesses,dead_chesses,
+                                                        settings.play_button)
+
+run_game()
---
+++ subboard.py
@@ -0,0 +1,29 @@
+from board import Board
+
+class LoseBoard(Board):
+    def __init__(self,settings,screen):
+        super().__init__(settings,screen)
+
+        self.settings = settings
+
+        msg='Not all pieces are unveiled. Are you sure to proceed?'
+        self.prep_text(msg)
+
+    def action(self):
+        self.settings.game_result = 'win'
+        self.settings.win_music_play = True
+        self.settings.game_end = True
+
+class TieBoard(Board):
+    def __init__(self,settings,screen):
+        super().__init__(settings,screen)
+
+        self.settings = settings
+
+        msg='Do you agree to make a tie?'
+        self.no_text = 'No'
+        self.prep_text(msg)
+    
+    def action(self):
+        self.settings.game_result = 'tie'
+        self.settings.game_end = True
